f77fcaf1abbd7d1575891f1eecd03b3caa7ae12c
Guilherme Portella <guilhermeportella2@gmail.com>
2021-10-21 13:22:05 -0300

    Ola mundo

---
+++ src/OlaMundo.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("Ola mundo")
+    ola()
+
+}
+
+
+fun ola(){
+
+    var str: String? = "Primeira função em kotlin"
+    println(str?.length)
+    println(str)
+
+    val ola: String = "Ola mundo 2"
+    println(ola)
+
+}
